7772db42e86a711ada66825aca93a3095871cd59
mkrastev <matey_krastev2@hotmail.com>
2024-05-19 23:13:22 +0200

    Adding some hyperparameters to test.

--- src/train.py
+++ src/train.py
@@ -252,12 +252,7 @@ def main():
         compute_metrics=functools.partial(compute_metrics, task=task, metric=metric),
     )
 
-    # initialize modules (if needed)
-    getattr(model, "init_modules", lambda: None)()
-    print('initialized modules')
-    model.print_trainable_parameters()
-
-    tick = time.perf_counter()
+    tick = time.time()
 
     class ProfCallback(TrainerCallback):
         def __init__(self, prof):
@@ -266,23 +261,25 @@ def main():
         def on_step_end(self, args, state, control, **kwargs):
             self.prof.step()
 
-    with torch.profiler.profile(
-        activities=[
-            torch.profiler.ProfilerActivity.CPU,
-            torch.profiler.ProfilerActivity.CUDA,
-        ],
-        schedule=torch.profiler.schedule(
-            skip_first=3, wait=1, warmup=1, active=4, repeat=6
-        ),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler(hf_args.output_dir),
-        profile_memory=True,
-        with_stack=True,
-        record_shapes=True,
-    ) as prof:
-        trainer.add_callback(ProfCallback(prof=prof))
-        trainer.train(resume_from_checkpoint=hf_args.resume_from_checkpoint)
-    trainer.save_model(os.path.join(hf_args.output_dir, 'final')) # otherwise it gets messy
-    print(f"Training took {time.perf_counter() - tick:.2f}s")
+    # with torch.profiler.profile(
+    #     activities=[
+    #         torch.profiler.ProfilerActivity.CPU,
+    #         torch.profiler.ProfilerActivity.CUDA,
+    #     ],
+    #     schedule=torch.profiler.schedule(
+    #         skip_first=3, wait=1, warmup=1, active=4, repeat=6
+    #     ),
+    #     on_trace_ready=torch.profiler.tensorboard_trace_handler(hf_args.output_dir),
+    #     profile_memory=True,
+    #     with_stack=True,
+    #     record_shapes=True,
+    # ) as prof:
+    #     trainer.add_callback(ProfCallback(prof=prof))
+    #     trainer.train()
+    trainer.train()
+    print(f"Training took {time.time() - tick:.1f}s")
+    
+    trainer.save_model(hf_args.output_dir)
 
 
 if __name__ == "__main__":
